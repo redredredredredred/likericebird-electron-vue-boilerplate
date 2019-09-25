746128f2fae739d1f66ec745c65624f2e7b9d989
Red Cloud <987060149@qq.com>
2019-09-25 20:54:18 +0800

    添加更新代码模块

--- auto-updater/src/background.js
+++ auto-updater/src/background.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow ,autoUpdater, dialog } from 'electron'
 import {
   createProtocol,
   installVueDevtools
@@ -71,6 +71,34 @@ app.on('ready', async () => {
 
   }
   createWindow()
+})
+
+// 更新APP 代码逻辑
+const server = 'https://your-deployment-url.com'
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL(feed)
+setInterval(() => {
+    autoUpdater.checkForUpdates()
+}, 60000)
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+
+    dialog.showMessageBox(dialogOpts, (response) => {
+        if (response === 0) autoUpdater.quitAndInstall()
+    })
+})
+
+autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
 })
 
 // Exit cleanly on request from parent process in development mode.
