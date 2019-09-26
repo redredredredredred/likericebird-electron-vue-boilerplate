e0270a6e101c5a01c4f39e4920e6410f5ee3f66a
Red Cloud <987060149@qq.com>
2019-09-26 21:02:18 +0800

    feat：添加系统托盘和关闭逻辑

--- ipc/src/background.js
+++ ipc/src/background.js
@@ -14,6 +14,7 @@ import {
     installVueDevtools
 } from "vue-cli-plugin-electron-builder/lib";
 import Badge from "electron-windows-badge";
+import electronLogger from 'electron-log';
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -21,6 +22,21 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 let tray;
+let badgeNum = 0
+
+// 主进程状态和配置管理
+let customConfig;
+if(process.platform === 'darwin'){
+    customConfig = {
+        logoPath: "icon-design-guide.png",
+        tipsLogoPath: "icon-design-guide.png"
+    }
+}else{
+    customConfig = {
+        logoPath: "favicon.ico",
+        tipsLogoPath: "favicon.ico"
+    }
+}
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
     {scheme: "app", privileges: {secure: true, standard: true}}
@@ -31,12 +47,62 @@ function createWindow() {
     win = new BrowserWindow({
         width: 800,
         height: 600,
-        icon: path.join(__static, "icon-design-guide.png"),
+        icon: path.join(__static, customConfig.logoPath),
         webPreferences: {
             nodeIntegration: true
         }
     });
 
+    // 在这里执行顺序很重要，如果在页面 loadURL 之后声明执行，将导致依赖的一些对象初始化时机过迟，报错
+
+    // 系统托盘，自定义操作
+    tray = new Tray(path.join(__static, customConfig.logoPath));
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: "devtools",
+            click: function (menuItem, browserWindow, event) {
+                electronLogger.info('tray openDevTools')
+                win.webContents.openDevTools()
+            }
+        },
+        {
+            label: "关闭窗口",
+            click: function (menuItem, browserWindow, event) {
+                electronLogger.info('tray 关闭窗口')
+                if (process.platform === 'darwin') {
+                    app.hide()
+                }else{
+                    win.hide()
+                }
+            }
+        },
+        {
+            label: "退出",
+            click: function (menuItem, browserWindow, event) {
+                electronLogger.info('tray 退出')
+                win.destroy()
+                app.quit()
+            }
+        }
+    ]);
+    tray.setToolTip("zhibei");
+    tray.setContextMenu(contextMenu);
+
+    tray.on('click', function (event) {
+        electronLogger.info('tray click')
+        win.show()
+        // 重新绑定徽章逻辑，因为在执行 close 生命周期的时候，win 被 destroy
+        const badgeOptions = {}
+        let newBagde = new Badge(win, badgeOptions);
+        newBagde.update(badgeNum)
+        badgeNum = 0
+    })
+
+    // 初始化 下角标提示
+    const badgeOptions = {}
+    new Badge(win, badgeOptions);
+
+    // 页面加载start
     if (process.env.WEBPACK_DEV_SERVER_URL) {
         // Load the url of the dev server if in development mode
         win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
@@ -46,55 +112,42 @@ function createWindow() {
         // Load the index.html when not in development
         win.loadURL("app://./index.html");
     }
+    // win.webContents.openDevTools()
 
-    // 初始化 下角标提示
-    const badgeOptions = {};
-    new Badge(win, badgeOptions);
 
-    // 系统托盘，自定义操作
-    tray = new Tray(path.join(__static, "icon-design-guide.png"));
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: "关闭窗口",
-            // type: "normal",
-            click: function (menuItem, browserWindow, event) {
-                win.hide()
-            },
-            role: "minimize"
-        },
-        {
-            label: "退出",
-            // type: "normal",
-            // click: function (menuItem, browserWindow, event) {
-            // },
-            role: "quit"
-        },
-    ]);
-    tray.setToolTip("Godap");
-    tray.setContextMenu(contextMenu);
-
-    tray.on('click', function (event) {
-        console.log('click', event)
-        win.show()
-    })
-    //
-    // tray.on('right-click', function (event) {
-    //     console.log('right-click', event)
-    // })
-    //
-    // tray.on('drop', function (event) {
-    //     console.log('drop', event)
-    // })
-    // win.on("closed", () => {
-    //     win = null;
-    //     app.hide()
-    // });
 
     win.on('close', (e) => {
+        electronLogger.info('win close')
         e.preventDefault();
-        win.hide()
-        return false;
+        if(win.isFocused()){
+            if (process.platform === 'darwin') {
+                app.hide()
+            }else{
+                win.hide()
+            }
+        }else{
+            app.quit()
+            app.exit()
+        }
+
+        e.returnValue = false
     });
+
+    // 处理外链打开逻辑
+    win.webContents.on('new-window', (e, url, frameName, disposition, options) => {
+        electronLogger.info('win new-window')
+        e.preventDefault()
+        shell.openExternal(url)
+        e.returnValue = false
+    })
+    // win.onbeforeunload = (e) => {
+    //     console.log('I do not want to be closed')
+    //
+    //     // 与通常的浏览器不同,会提示给用户一个消息框,
+    //     //返回非空值将默认取消关闭
+    //     //建议使用对话框 API 让用户确认关闭应用程序.
+    //     e.returnValue = false // 相当于 `return false` ，但是不推荐使用
+    // }
     // win.on('closed', (e) => {
     //     e.preventDefault();
     //     return false;
@@ -147,6 +200,17 @@ ipcMain.once("beep-once", (event, text) => {
     shell.beep();
     event.returnValue = ""; // sendSync
 });
+
+
+ipcMain.on("update-badge", function (event, num) {
+    badgeNum = num
+    if(num){
+        tray.setImage(path.join(__static, "favicon1.png"))
+    }else{
+        tray.setImage(path.join(__static, "favicon.png"))
+    }
+    event.returnValue = 'info'
+})
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
